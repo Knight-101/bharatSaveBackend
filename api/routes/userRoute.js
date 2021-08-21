785a62a13daf541aefbfcd5a005ca604627cd941
Hersh Kumar <56927116+unc0ded@users.noreply.github.com>
2021-08-22 03:50:27 +0530

    rearrange route structure for user balance data

--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -3,6 +3,8 @@ const controller = require('../controllers/user.controller');
 
 router.get('/', controller.getUserDetails);
 
+router.get('/balance', controller.getBalanceDetails);
+
 router.get('/bankDetails', controller.getBankDetails);
 
 module.exports = router;
